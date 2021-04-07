f3297be8233b9b576bbb7ce9bf7af39eadc528e2
Viktor Mouton <vmouton@insat.insa-toulouse.fr>
2021-04-07 15:12:07 +0200

    Sortie Son

--- Assemblage/PjtKEIL_StepSon/Src/GestionSon.s
+++ Assemblage/PjtKEIL_StepSon/Src/GestionSon.s
@@ -1,7 +1,7 @@
 	PRESERVE8
 	THUMB   
 		
-
+	import Son
 ; ====================== zone de réservation de données,  ======================================
 ;Section RAM (read only) :
 	area    mesdata,data,readonly
@@ -9,9 +9,10 @@
 
 ;Section RAM (read write):
 	area    maram,data,readwrite
-		
-
-	
+SortieSon dcw 0
+Iterateur dcd 0
+	export CallbackSon
+	export SortieSon
 ; ===============================================================================================
 	
 
@@ -19,12 +20,28 @@
 		
 ;Section ROM code (read only) :		
 	area    moncode,code,readonly
-; écrire le code ici		
+; écrire le code ici
 
-
-
-
-
-		
-		
+CallbackSon proc
+	ldr R0,=Son
+	ldr R3,=Iterateur
+	ldr R3,[R3]
+	ldr R1,=5512
+	ldr R1,[R1]
+	cmp R3,R1
+	beq fin
+	ldrsh R0,[R0,R3]
+	ldr R2,=SortieSon
+	add R0,#32768
+	mov R1,#91
+	sdiv R0,R0,R1
+	str R0,[R2]
+	
+	ldr R3,=Iterateur
+	ldr R1,[R3]
+	add R1,#2
+	str R1,[R3]
+fin
+	bx lr
+	endp
 	END	
--- Assemblage/PjtKEIL_StepSon/Src/principal.c
+++ Assemblage/PjtKEIL_StepSon/Src/principal.c
@@ -2,7 +2,8 @@
 
 #include "DriverJeuLaser.h"
 
-
+void CallbackSon(void);
+int PeriodeSonMicrosec;
 
 int main(void)
 {
@@ -13,7 +14,8 @@ int main(void)
 
 // Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
 CLOCK_Configure();
-
+Active_IT_Debordement_Timer( TIM4, 2, CallbackSon );
+Timer_1234_Init_ff( TIM4, PeriodeSonMicrosec*72);
 
 	
 	
